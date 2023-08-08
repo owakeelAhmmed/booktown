f84fe1ab58ff0df925140c861193bda395010a6c
owakeelAhmmed <owakeel.ahmmed@gmail.com>
2023-08-09 02:10:24 +0600

    home page add to cart button hidden done

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,13 +2,15 @@
 import { IProduct } from '@/types/globalTypes';
 import { toast } from './ui/use-toast';
 import { Button } from './ui/button';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 interface IProps {
   product: IProduct;
 }
 
 export default function ProductCard({ product }: IProps) {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
   const handleAddProduct = (product: IProduct) => {
     toast({
       description: 'Product Added',
@@ -16,7 +18,7 @@ export default function ProductCard({ product }: IProps) {
   };
   return (
     <div>
-      <div className="rounded-2xl h-[480px] flex flex-col items-start justify-between p-5 overflow-hidden shadow-md border border-gray-100 hover:shadow-2xl hover:scale-[102%] transition-all gap-2">
+      <div className="rounded-2xl h-[580px] flex flex-col items-start justify-between p-5 overflow-hidden shadow-md border border-gray-100 hover:shadow-2xl hover:scale-[102%] transition-all gap-2">
         <Link to={`/product-details/${product._id}`} className="w-full">
           <img src={product?.image} alt="product" />
           <h1 className="text-xl font-semibold">{product?.name}</h1>
@@ -26,9 +28,11 @@ export default function ProductCard({ product }: IProps) {
           Availability: {product?.status ? 'In stock' : 'Out of stock'}
         </p>
         <p className="text-sm">Price: {product?.price}</p>
-        <Button variant="default" onClick={() => handleAddProduct(product)}>
-          Add to cart
-        </Button>
+        {!isHomePage && (
+          <Button variant="default" onClick={() => handleAddProduct(product)}>
+            Add to cart
+          </Button>
+        )}
       </div>
     </div>
   );
